293282479bb6703ca8c99f18404d4206ccac2e3e
Sonny <sonnyasdev@gmail.com>
2025-07-20 23:25:10 +0200

    chore: remove useless stuff and add mantine

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+import { Title } from "@mantine/core";
+
+function App() {
+  return (
+    <>
+      <Title order={1}>Hello World</Title>
+    </>
+  );
+}
+
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { createRoot } from "react-dom/client";
+import App from "./app.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <MantineProvider>
     <App />
-  </StrictMode>,
-)
+  </MantineProvider>
+);
